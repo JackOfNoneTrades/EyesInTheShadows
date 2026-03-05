6a237d09453319958f56d2d63d753a1538d03f87
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-03-05 16:46:04 +0100

    fixed eye glowing

--- src/main/java/org/fentanylsolutions/eyesintheshadows/client/ModelEyes.java
+++ src/main/java/org/fentanylsolutions/eyesintheshadows/client/ModelEyes.java
@@ -189,7 +189,7 @@ public class ModelEyes extends ModelBase {
         }
 
         GL11.glEnable(GL11.GL_BLEND);
-        GL11.glEnable(GL11.GL_ALPHA_TEST);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDepthMask(false);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
