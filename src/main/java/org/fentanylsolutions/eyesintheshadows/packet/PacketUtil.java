20f63060f8abb7612a917ac244f1b9e5db0c903f
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-03-05 14:37:34 +0100

    don't crash constructor on fake worlds

--- src/main/java/org/fentanylsolutions/eyesintheshadows/packet/PacketUtil.java
+++ src/main/java/org/fentanylsolutions/eyesintheshadows/packet/PacketUtil.java
@@ -10,15 +10,15 @@ public class PacketUtil {
 
     public static void sendEntitySyncPacketToClient(IModEntity parEntity) {
         Entity theEntity = (Entity) parEntity;
-        if (!theEntity.worldObj.isRemote) {
-            // // DEBUG
-            // System.out.println("sendEntitySyncPacket from server");
-            // TODO: maybe sync only what is necessary, not everything at the same time
-            ((WorldServer) theEntity.worldObj).getEntityTracker()
-                .func_151248_b(
-                    theEntity,
-                    PacketHandler.net.getPacketFrom(
-                        new MessageSyncEntityToClient(theEntity.getEntityId(), parEntity.getSyncDataCompound())));
+        if (!(theEntity.worldObj instanceof WorldServer)) {
+            return;
         }
+
+        // TODO: maybe sync only what is necessary, not everything at the same time
+        ((WorldServer) theEntity.worldObj).getEntityTracker()
+            .func_151248_b(
+                theEntity,
+                PacketHandler.net.getPacketFrom(
+                    new MessageSyncEntityToClient(theEntity.getEntityId(), parEntity.getSyncDataCompound())));
     }
 }
